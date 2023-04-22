a52baa7ba2d61e4797982386f0eb8762741faacf
Second Chen <second_chen@trendmicro.com>
2023-04-22 21:26:58 +0800

    fix a bug

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -153,7 +153,7 @@ M.config = function()
                 -- OPTIONAL:
                 --   `nvim-notify` is only needed, if you want to use the notification view.
                 --   If not available, we use `mini` as the fallback
-                "rcarriga/nvim-notify",
+                -- "rcarriga/nvim-notify",
             },
         },
         {
