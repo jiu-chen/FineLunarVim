11810af7bd2e34fac019a34ac95b9a9dcec76cc9
Second Chen <second_chen@trendmicro.com>
2023-04-22 14:58:40 +0800

    rich my config

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -66,6 +66,13 @@ M.config = function()
         {
             "mg979/vim-visual-multi",
         },
+        {
+            "tpope/vim-surround",
+            -- make sure to change the value of `timeoutlen` if it's not triggering correctly, see https://github.com/tpope/vim-surround/issues/117
+            -- setup = function()
+            --  vim.o.timeoutlen = 500
+            -- end
+        },
         --------------
         -- 语言服务 --
         --------------
@@ -115,6 +122,40 @@ M.config = function()
         --------------
         -- 界面元素 --
         --------------
+        {
+            "folke/noice.nvim",
+            config = function()
+                require("noice").setup({
+                    lsp = {
+                        signature = {
+                            enabled = false,
+                        },
+                        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+                        override = {
+                            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                            ["vim.lsp.util.stylize_markdown"] = true,
+                            ["cmp.entry.get_documentation"] = true,
+                        },
+                    },
+                    -- you can enable a preset for easier configuration
+                    presets = {
+                        bottom_search = true,         -- use a classic bottom cmdline for search
+                        command_palette = true,       -- position the cmdline and popupmenu together
+                        long_message_to_split = true, -- long messages will be sent to a split
+                        inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+                        lsp_doc_border = false,       -- add a border to hover docs and signature help
+                    },
+                })
+            end,
+            requires = {
+                -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+                "MunifTanjim/nui.nvim",
+                -- OPTIONAL:
+                --   `nvim-notify` is only needed, if you want to use the notification view.
+                --   If not available, we use `mini` as the fallback
+                "rcarriga/nvim-notify",
+            },
+        },
         {
             "windwp/nvim-ts-autotag",
             config = function()
--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,35 +1,36 @@
 local M = {}
 
 M.config = function()
-	lvim.builtin.treesitter.autotag.enable = true
-	lvim.builtin.treesitter.rainbow.enable = true
-	lvim.builtin.treesitter.ensure_installed = {
-		"bash",
-		"vim",
-		"lua",
-		"c",
-		"make",
-		"cpp",
-		"cmake",
-		"go",
-		"gomod",
-		"python",
-		"javascript",
-		"typescript",
-		"tsx",
-		"http",
-		"html",
-		"css",
-		"markdown",
-		"json",
-		"jsonc",
-		"yaml",
-		"toml",
-		"comment",
-		"regex",
-		"dockerfile",
-		"proto",
-	}
+    lvim.builtin.treesitter.autotag.enable = true
+    lvim.builtin.treesitter.rainbow.enable = true
+    lvim.builtin.treesitter.ensure_installed = {
+        "bash",
+        "vim",
+        "lua",
+        "c",
+        "make",
+        "cpp",
+        "cmake",
+        "go",
+        "gomod",
+        "python",
+        "javascript",
+        "typescript",
+        "tsx",
+        "http",
+        "html",
+        "css",
+        "markdown",
+        "markdown_inline",
+        "json",
+        "jsonc",
+        "yaml",
+        "toml",
+        "comment",
+        "regex",
+        "dockerfile",
+        "proto",
+    }
 end
 
 return M
